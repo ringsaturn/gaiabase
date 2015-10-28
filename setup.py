5ceac08c7bfecfc61394bcc40ded4f27235578f1
Yan Yang <dimply@126.com>
2015-10-28 14:35:28 +0800

    ommit opening README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='gaiabase',
-    version='0.0.1-beta',
+    version='0.0.2',
     author='Mingli Yuan',
     author_email='mingli.yuan@gmail.com',
     packages=['gaia','gaia.cloud','gaia.db'],
@@ -10,7 +10,6 @@ setup(
     url='git+https://github.com/caiyunapp/gaiabase.git',
     license='LICENSE',
     description='The base library for gaia-based projects',
-    long_description=open('README.md').read(),
     install_requires=[
         "PyYAML"
     ],
