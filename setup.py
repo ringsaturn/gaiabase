86d57930c0bdb40d2f4104a33fc16b766d577eee
Mingli Yuan <mingli.yuan@gmail.com>
2015-10-28 13:55:32 +0800

    fix version before publish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='gaiabase',
-    version='0.1.0',
+    version='0.0.1-alpha',
     author='Mingli Yuan',
     author_email='mingli.yuan@gmail.com',
     packages=['gaia','gaia.cloud','gaia.db'],
