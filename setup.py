3597a9c5d08a964f56ac822e24e4bc5d50e8574e
Mingli Yuan <mingli.yuan@gmail.com>
2016-05-19 16:32:32 +0800

    fix var name

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='gaiabase',
-    version='0.0.5',
+    version='0.0.6',
     author='Mingli Yuan',
     author_email='mingli.yuan@gmail.com',
     packages=['gaia','gaia.cloud','gaia.db'],
     package_dir={'gaia': 'gaia'},
-    url='git+https://github.com/caiyunapp/gaiabase.git',
+    url='https://github.com/caiyunapp/gaiabase.git',
     license='LICENSE',
     description='The base library for gaia-based projects',
     install_requires=[
